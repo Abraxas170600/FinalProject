1d90f5aed34d8653845da90c72b27b38c8bb62d4
Abraxas170600 <abraxassabaoth666@gmail.com>
2024-12-03 23:50:49 -0500

    Enemy system

---
+++ Assets/Scripts/Entities/Enemy/Enemy.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class Enemy : Entity
+{
+    [Header("Dependences")]
+    protected Player player;
+
+    [Header("Attributes")]
+    [SerializeField] protected float speed;
+
+    [Header("Player Detector")]
+    [SerializeField] protected float radio = 1.0f;
+    protected override void Start()
+    {
+        base.Start();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+    protected override void Defeat()
+    {
+        base.Defeat();
+        Destroy(gameObject);
+    }
+    protected override void Movement()
+    {
+        if (PlayerDetected())
+            Aggresive();
+        else
+            Passive();
+    }
+    protected abstract void Passive();
+    protected abstract void Aggresive();
+    protected bool PlayerDetected()
+    {
+        float distanceFromPlayer = Vector2.Distance(player.transform.position, transform.position);
+        return distanceFromPlayer < radio;
+    }
+    protected void Flip(Vector2 direction)
+    {
+        bool movingRight = direction.x > 0;
+        bool facingRight = transform.localScale.x > 0;
+
+        if (movingRight != facingRight)
+        {
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radio);
+    }
+}
